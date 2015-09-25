7497569435f2bdd4b10a4e6ce210d9fc2c9dc037
Vinicius Bandeira <bandeiravinicius0@live.com>
2015-09-25 09:14:26 -0300

    primeiro commit - tutorial parte 3- incompleto

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = "
+        + genre);
+
+            return message;
+        }
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+    }
+}
